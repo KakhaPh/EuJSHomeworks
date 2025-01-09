a7ff585bbff82de0720d5415398172c75a765630
KakhaPh <kakha.putkaradze@eu.edu.ge>
2025-01-09 23:34:15 +0400

    JS_Homework_7

---
+++ Homework7 (Kakha Phutkaradze)/app.js
@@ -0,0 +1,9 @@
+// პირველი დავალება
+
+// const text = '{ }';
+// const obj = JSON.parse(text);
+// obj.age = eval("(" + obj.txt + ")");
+// document.getElementById("demo").innerHTML = obj.name + ", " + obj.txt();
+
+// მეორე დავალება
+console.log(document.body.innerText);
